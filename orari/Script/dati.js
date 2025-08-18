5ea05566b91f23da2356d087744c2983bda72eb8
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2025-08-18 15:31:06 +0200

    Io odio Google Fogli

--- orari/Script/dati.js
+++ orari/Script/dati.js
@@ -1,14 +1,7 @@
 function getDati(){
     let out = [
-        "{persona: ""Lorenzo"", materia: ""Interazione Uomo-Macchina"", aula: ""Aula Magna di Matematica"", giorno: ""Lunedì"", inizio: 132, fine: 189},"
-"{persona: ""Lorenzo"", materia: ""Interazione Uomo-Macchina"", aula: ""Laboratorio T"", giorno: ""Lunedì"", inizio: 198, fine: 204},"
-"{persona: ""Lorenzo"", materia: ""Basi di Dati"", aula: ""Aula Magna di Matematica"", giorno: ""Martedì"", inizio: 108, fine: 165},"
-"{persona: ""Lorenzo"", materia: ""Basi di Dati"", aula: ""Laboratorio T"", giorno: ""Martedì"", inizio: 132, fine: 186},"
-"{persona: ""Lorenzo"", materia: ""Basi di Dati"", aula: ""Aula Magna di Matematica"", giorno: ""Mercoledì"", inizio: 108, fine: 165},"
-"{persona: ""Lorenzo"", materia: ""Linguaggi di Programmazione"", aula: ""Aula Magna di Matematica"", giorno: ""Mercoledì"", inizio: 132, fine: 189},"
-"{persona: ""Lorenzo"", materia: ""Linguaggi di Programmazione"", aula: ""Laboratorio T"", giorno: ""Mercoledì"", inizio: 198, fine: 204},"
-"{persona: ""Lorenzo"", materia: ""Linguaggi di Programmazione"", aula: ""Aula Magna di Matematica"", giorno: ""Giovedì"", inizio: 108, fine: 165},"
-"{persona: ""Lorenzo"", materia: ""Linguaggi di Programmazione"", aula: ""Laboratorio T"", giorno: ""Giovedì"", inizio: 132, fine: 186},"
+       {persona: "Lorenzo", materia: "Interazione Uomo-Macchina", aula: "Aula Magna di Matematica", giorno: "Lunedì", inizio: 132, fine: 189},{persona: "Lorenzo", materia: "Interazione Uomo-Macchina", aula: "Laboratorio T", giorno: "Lunedì", inizio: 198, fine: 204},{persona: "Lorenzo", materia: "Basi di Dati", aula: "Aula Magna di Matematica", giorno: "Martedì", inizio: 108, fine: 165},{persona: "Lorenzo", materia: "Basi di Dati", aula: "Laboratorio T", giorno: "Martedì", inizio: 132, fine: 186},{persona: "Lorenzo", materia: "Basi di Dati", aula: "Aula Magna di Matematica", giorno: "Mercoledì", inizio: 108, fine: 165},{persona: "Lorenzo", materia: "Linguaggi di Programmazione", aula: "Aula Magna di Matematica", giorno: "Mercoledì", inizio: 132, fine: 189},{persona: "Lorenzo", materia: "Linguaggi di Programmazione", aula: "Laboratorio T", giorno: "Mercoledì", inizio: 198, fine: 204},{persona: "Lorenzo", materia: "Linguaggi di Programmazione", aula: "Aula Magna di Matematica", giorno: "Giovedì", inizio: 108, fine: 165},{persona: "Lorenzo", materia: "Linguaggi di Programmazione", aula: "Laboratorio T", giorno: "Giovedì", inizio: 132, fine: 186}
+
 ];
     return out;
 }
