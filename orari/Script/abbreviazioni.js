e376bf9e71aa7811292c91e9039f9a3acfe4b1b2
Eic17H <eic17h+github@gmail.com>
2025-09-29 15:38:10 +0200

    Regexate certe abbreviazioni

--- orari/Script/abbreviazioni.js
+++ orari/Script/abbreviazioni.js
@@ -1,10 +1,5 @@
 function abbreviaMaterie(nome){
     switch(nome){
-        case 'Traduzione Inglese 1': return 'TrEng1';
-        case 'Inglese 2 Ex': return 'Eng2-Ex';
-        case 'Tedesco 2 Ex': return 'Deu2-Ex';
-        case 'Tedesco 2': return 'Deu2';
-        case 'Traduzione Tedesca 1': return 'TrDeu1';
         case 'Programmazione 2': return 'PR2';
         case 'Reti di Calcolatori': return 'Reti';
         case 'Calcolo Scientifico e Metodi Numerici': return 'CSMN';
@@ -13,18 +8,11 @@ function abbreviaMaterie(nome){
         case 'Reti di Calcolatori Ex': return 'Reti-Ex';
         case 'Fondamenti di Programmazione Web': return 'FPW';
         case 'Programmazione 2 Ex': return 'PR2-Ex';
-        case 'Cinese 2 Ex': return 'Cmn2-Ex';
         case 'Com. e Gior. nell\'Età Contemp.': return 'ComGior';
-        case 'Cinese 2': return 'Cmn2';
         case 'Teoria e Tecnica del Ling. Cinem.': return 'Cinema';
-        case 'Cinese B2': return 'CmnB2';
         case 'Didat. dei Proc. Educativi e Formativi': return 'DPEF';
         case 'Linguistica Italiana 2': return 'LingIta2';
         case 'Linguistica Italiana 1': return 'LingIta1';
-        case 'Spagnolo 1 Ex': return 'Spa1-Ex';
-        case 'Spagnolo 1': return 'Spa1';
-        case 'Inglese 1 Ex': return 'Eng1-Ex';
-        case 'Inglese 1': return 'Eng1';
         case 'Litologia e Geologia': return 'LitGeo';
         case 'Geometria e Algebra': return 'GeomAlg';
         case 'Disegno Ex': return 'Disegno';
@@ -35,22 +23,23 @@ function abbreviaMaterie(nome){
         case 'Interazione Uomo-Macchina': return 'IUM';
         case 'Basi di Dati': return 'BdD';
         case 'Linguaggi di Programmazione': return 'LdP';
-        case 'Russo 1 Ex': return 'Rus1-Ex';
-        case 'Russo 1': return 'Rus1';
         case 'Laboratorio Cinese Ex': return 'LabCmn-Ex';
         case 'Laboratorio Cinese': return 'LabCmn';
-        case 'Inglese Ex': return 'Eng-Ex';
-        case 'Inglese': return 'Eng';
         case 'English Language and Communication Skills': return 'EngSkill';
         case 'Linguaggi dello Spettacolo': return 'Spett';
-        case 'Tedesco 3 Ex': return 'Deu3-Ex';
-        case 'Tedesco 3': return 'Deu3';
-        case 'Inglese 3 Ex': return 'Eng3-Ex';
         case 'Filologia Germanica': return 'FilGem';
         case 'Mediazione Orale Inglese': return 'MedEng';
     }
     let regexRules = [
-        [/.*\/.*/, "..."]
+        [/.*\/.*/, "..."],
+        [/Tedesco(( )([0-9]))?/, "Deu$3"],
+        [/Cinese(( )([0-9]))?/, "Cmn$3"],
+        [/Inglese(( )([0-9]))?/, "Eng$3"],
+        [/Spagnolo(( )([0-9]))?/, "Spa$3"],
+        [/Russo(( )([0-9]))?/, "Rus$3"],
+        [/Traduzione (.*)/, "Tr$1"],
+        [/Laboratorio (.*)/, "Lab$1"],
+        [/(.+) Ex/, "$1-Ex"],
     ]
     let i
     for(i in regexRules) nome = nome.replace(regexRules[i][0], regexRules[i][1])
