b427c109d4f6c901df66a085d96316c262c05239
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2025-09-30 11:35:54 +0200

    Regexizzate più abbreviazioni

--- orari/Script/abbreviazioni.js
+++ orari/Script/abbreviazioni.js
@@ -1,13 +1,5 @@
 function abbreviaMaterie(nome){
     switch(nome){
-        case 'Programmazione 2': return 'PR2';
-        case 'Reti di Calcolatori': return 'Reti';
-        case 'Calcolo Scientifico e Metodi Numerici': return 'CSMN';
-        case 'Calcolo Scientifico e Metodi Numerici Ex': return 'CSMN-Ex';
-        case 'Fondamenti di Programmazione Web Ex': return 'FPW-Ex';
-        case 'Reti di Calcolatori Ex': return 'Reti-Ex';
-        case 'Fondamenti di Programmazione Web': return 'FPW';
-        case 'Programmazione 2 Ex': return 'PR2-Ex';
         case 'Com. e Gior. nell\'Età Contemp.': return 'ComGior';
         case 'Teoria e Tecnica del Ling. Cinem.': return 'Cinema';
         case 'Didat. dei Proc. Educativi e Formativi': return 'DPEF';
@@ -20,17 +12,19 @@ function abbreviaMaterie(nome){
         case 'Fondamenti di Informatica': return 'FDI';
         case 'Tecnologia dei Materiali': return 'TDM';
         case 'Fisica 2': return 'Fis2';
-        case 'Interazione Uomo-Macchina': return 'IUM';
-        case 'Basi di Dati': return 'BdD';
-        case 'Linguaggi di Programmazione': return 'LdP';
-        case 'Laboratorio Cinese Ex': return 'LabCmn-Ex';
-        case 'Laboratorio Cinese': return 'LabCmn';
         case 'English Language and Communication Skills': return 'EngSkill';
         case 'Linguaggi dello Spettacolo': return 'Spett';
         case 'Filologia Germanica': return 'FilGem';
         case 'Mediazione Orale Inglese': return 'MedEng';
     }
     let regexRules = [
+        ["Programmazione 2", "PR2"],
+        ["Reti di Calcolatori", "Reti"],
+        ["Calcolo Scientifico e Metodi Numerici", "CSMN"],
+        ["Fondamenti di Programmazione Web", "FPW"],
+        ["Interazione Uomo-Macchina", "IUM"],
+        ["Basi di Dati", "BD"],
+        ["Linguaggi di Programmazione", "LiP"],
         [/.*\/.*/, "..."],
         [/Tedesco(( )([0-9]))?/, "Deu$3"],
         [/Cinese(( )([0-9]))?/, "Cmn$3"],
