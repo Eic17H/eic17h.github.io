5bed572ff53cde9ce8137619ba2aeab76a0250f4
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2025-10-01 11:21:06 +0200

    Lezione finta???

--- orari/Script/dati.js
+++ orari/Script/dati.js
@@ -6,7 +6,7 @@ function getDati() {
         {persona: "Lorenzo",materia: "Basi di Dati Ex",aula: "Laboratorio T",giorno: "Martedì",inizio: 132,fine: 162},
         {persona: "Lorenzo",materia: "Basi di Dati",aula: "Aula Magna di Matematica",giorno: "Mercoledì",inizio: 108,fine: 129},
         {persona: "Lorenzo",materia: "Linguaggi di Programmazione",aula: "Aula Magna di Matematica",giorno: "Mercoledì",inizio: 132,fine: 153},
-        {persona: "Lorenzo",materia: "Linguaggi di Programmazione Ex",aula: "Laboratorio T",giorno: "Mercoledì",inizio: 174,fine: 204},
+        //{persona: "Lorenzo",materia: "Linguaggi di Programmazione Ex",aula: "Laboratorio T",giorno: "Mercoledì",inizio: 174,fine: 204},
         {persona: "Lorenzo",materia: "Linguaggi di Programmazione",aula: "Aula Magna di Matematica",giorno: "Giovedì",inizio: 108,fine: 129},
         {persona: "Lorenzo",materia: "Linguaggi di Programmazione Ex",aula: "Laboratorio T",giorno: "Giovedì",inizio: 132,fine: 162},
         {persona: "Swami",materia: "Russo 1 Ex",aula: "Aula 4",giorno: "Lunedì",inizio: 197,fine: 215},
