b852a7b59b642bac83fb92581b04d7e9a3677581
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2025-10-01 10:20:20 +0200

    aggiunta lezione contemporanea

--- orari/Script/dati.js
+++ orari/Script/dati.js
@@ -17,7 +17,7 @@ function getDati() {
         {persona: "Swami",materia: "Russo 1 Ex",aula: "Aula 4",giorno: "Martedì",inizio: 197,fine: 215},
         {persona: "Swami",materia: "Laboratorio Cinese Ex",aula: "Aula 2",giorno: "Mercoledì",inizio: 121,fine: 139},
         {persona: "Swami",materia: "Inglese Ex",aula: "Laboratorio Lingue A1",giorno: "Mercoledì",inizio: 140,fine: 158},
-        {persona: "Swami",materia: "English Language and Communication Skills",aula: "Aula 7",giorno: "Mercoledì",inizio: 158,fine: 177},
+        {persona: "Swami",materia: "English Language and Communication Skills / Russo 1",aula: "Aula 7 / Aula 6",giorno: "Mercoledì",inizio: 158,fine: 177},
         {persona: "Swami",materia: "Linguaggi dello Spettacolo",aula: "Aula 9",giorno: "Mercoledì",inizio: 178,fine: 196},
         {persona: "Swami",materia: "Laboratorio Cinese",aula: "Aula 4",giorno: "Giovedì",inizio: 121,fine: 139},
         {persona: "Swami",materia: "Russo 1 Ex",aula: "Aula 6",giorno: "Giovedì",inizio: 178,fine: 196},
