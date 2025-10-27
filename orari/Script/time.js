d1735e7d768371f9e157a8faafbd0b231fad621a
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2025-10-27 12:08:49 +0100

    Aggiunto parser da stringa orario a int. Molto spaghetti code

--- orari/Script/time.js
+++ orari/Script/time.js
@@ -6,13 +6,19 @@ function oraNumero(input){
         ora = input.replace(/:.+/,"")
     }
     // Formato 1u = 5min
-	else if(String(input).match(/^[0-9]+$/)) {
+		else if(String(input).match(/^[0-9]+$/)) {
     	let minuti = input*5
     	minuto = minuti%60
     	ora = (minuti-minuto)/60   
     }
-	else return null
-	return "" + String(ora).padStart(2,'0') + ":" + String(minuto).padStart(2,'0') + ""
+		else return null
+		return "" + String(ora).padStart(2,'0') + ":" + String(minuto).padStart(2,'0') + ""
+}
+
+function oraInt(input) {
+		let input = oraNumero(input)
+    if(input == null) return null
+    return input.replace(/.+:/, "")/5 + input.replace(/:.+/,"")*12
 }
 
 // inizio ZWSP - ZWSP fine
