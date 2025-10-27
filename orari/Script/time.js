c3122f228b1235cacff532bc9e48669c3caf82ee
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2025-10-27 11:06:16 +0100

    Aggiunto nuovo formato per l'orario
    Ora supporta hh:mm e restituisce null se l'input non è valido. Ora verrà meglio modificare i dati direttamente dal file

--- orari/Script/time.js
+++ orari/Script/time.js
@@ -1,12 +1,21 @@
 function oraNumero(input){
-    let minuti = input*5
-    let minuto = minuti%60
-    let ora = (minuti-minuto)/60
-    return "" + String(ora).padStart(2,'0') + ":" + String(minuto).padStart(2,'0') + ""
+  	let minuto, ora
+    // Formato hh:mm
+    if(String(input).match(/^[0-2][0-9]?:[0-5][0-9]$/)) {
+        minuto = input.replace(/.+:/, "")
+        ora = input.replace(/:.+/,"")
+    }
+    // Formato 1u = 5min
+	else if(String(input).match(/^[0-9]+$/)) {
+    	let minuti = input*5
+    	minuto = minuti%60
+    	ora = (minuti-minuto)/60   
+    }
+	else return null
+	return "" + String(ora).padStart(2,'0') + ":" + String(minuto).padStart(2,'0') + ""
 }
 
 // inizio ZWSP - ZWSP fine
 function orariox(input){
     if(input.materia!="") return oraNumero(input.inizio) + "-​" + oraNumero(input.fine)
     else return ""
-}
