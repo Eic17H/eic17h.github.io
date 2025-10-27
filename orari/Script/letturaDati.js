cffa68478ce3cb80dce454040dc9b0f6393dffee
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2025-10-27 12:23:42 +0100

    Mi arrendo. Ci penserò dopo perché non trova la funzione dall'altro file

--- orari/Script/letturaDati.js
+++ orari/Script/letturaDati.js
@@ -1,5 +1,39 @@
 import { oraInt, oraNumero, orariox } from './time.js';
 
+
+// TODO: rimuovere questo blocco sotto
+
+export function oraNumero(input){
+  	let minuto, ora
+    // Formato hh:mm
+    if(String(input).match(/^[0-2][0-9]?:[0-5][0-9]$/)) {
+        minuto = input.replace(/.+:/, "")
+        ora = input.replace(/:.+/,"")
+    }
+    // Formato 1u = 5min
+		else if(String(input).match(/^[0-9]+$/)) {
+    	let minuti = input*5
+    	minuto = minuti%60
+    	ora = (minuti-minuto)/60   
+    }
+		else return null
+		return "" + String(ora).padStart(2,'0') + ":" + String(minuto).padStart(2,'0') + ""
+}
+
+export function oraInt(input) {
+		let input = oraNumero(input)
+    if(input == null) return null
+    return input.replace(/.+:/, "")/5 + input.replace(/:.+/,"")*12
+}
+
+// inizio ZWSP - ZWSP fine
+export function orariox(input){
+    if(input.materia!="") return oraNumero(input.inizio) + "-​" + oraNumero(input.fine)
+    else return ""
+}
+
+// TODO: rimuovere questo blocco sopra
+
 function loadData(oggi){
     // Ok questa cosa funziona per leggere il nome dei campi dalla stringa
 
