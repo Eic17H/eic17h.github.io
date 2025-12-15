a3e4fa222ab14ae11dc4ef3d1bbb31d74e6aa147
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2025-12-15 11:09:09 +0100

    Aula CREA arancione

--- orari/Script/estetica.js
+++ orari/Script/estetica.js
@@ -71,6 +71,7 @@ function coloreEdificio(edificio) {
         case "Palazzo delle Scienze": return "var(--red)";
         case "Ingegneria": return "var(--green)";
         case "Sa Duchessa": return "var(--blue)";
+        case "CREA": return "var(--orange)";
     }
     return null
 }
