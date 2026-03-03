6f598409d7eda986381f365165cbff060c5a3c5b
Eiĉ <139933730+Eic17H@users.noreply.github.com>
2026-03-03 11:42:47 +0100

    Orari di Vargiu

--- orari/Script/dati.js
+++ orari/Script/dati.js
@@ -1,13 +1,22 @@
 function getDati() {
     let out = [
-        {persona: "Lorenzo", materia: "Istituzioni di Logica", aula: "Aula 3A", edificio: "Sa Duchessa", giorno: "Lunedì", inizio: "11:40", fine: "13:05"}, 
-        {persona: "Lorenzo", materia: "Ingegneria del Software", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Martedì", inizio: "9:00", fine: "10:45"}, 
+        {persona: "Lorenzo", materia: "Istituzioni di Logica", aula: "Aula 3A", edificio: "Sa Duchessa", giorno: "Lunedì", inizio: "11:40", fine: "13:05"},
+        {persona: "Lorenzo", materia: "Ingegneria del Software", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Martedì", inizio: "9:00", fine: "10:45"},
         {persona: "Lorenzo", materia: "Ingegneria del Software Ex", aula: "Laboratorio T", edificio: "Palazzo delle Scienze", giorno: "Martedì", inizio: "11:00", fine: "13:00"},
         {persona: "Lorenzo", materia: "Ingegneria del Software", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Mercoledì", inizio: "9:00", fine: "10:45"},
         {persona: "Lorenzo", materia: "Istituzioni di Logica", aula: "Aula 17", edificio: "Sa Duchessa", giorno: "Mercoledì", inizio: "11:40", fine: "13:05"},
-        {persona: "Lorenzo", materia: "Calcolo Scientifico e Metodi Numerici", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Mercoledì", inizio: "14:30", fine: "16:15"}, 
-        {persona: "Lorenzo", materia: "Istituzioni di Logica", aula: "Aula 17", edificio: "Sa Duchessa", giorno: "Venerdì", inizio: "9:15", fine: "10:50"}, 
-        {persona: "Lorenzo", materia: "Calcolo Scientifico e Metodi Numerici Ex", aula: "Laboratorio T", edificio: "Palazzo delle Scienze", giorno: "Venerdì", inizio: "11:30", fine: "13:15"}, 
+        {persona: "Lorenzo", materia: "Calcolo Scientifico e Metodi Numerici", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Mercoledì", inizio: "14:30", fine: "16:15"},
+        {persona: "Lorenzo", materia: "Istituzioni di Logica", aula: "Aula 17", edificio: "Sa Duchessa", giorno: "Venerdì", inizio: "9:15", fine: "10:50"},
+        {persona: "Lorenzo", materia: "Calcolo Scientifico e Metodi Numerici Ex", aula: "Laboratorio T", edificio: "Palazzo delle Scienze", giorno: "Venerdì", inizio: "11:30", fine: "13:15"},
+        {persona: "Vargiu", materia: "Matematica Discreta", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Lunedì", inizio: "9:00", fine: "10:45"},
+        {persona: "Vargiu", materia: "Fisica e Metodo Scientifico", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Lunedì", inizio: "11:00", fine: "12:45"},
+        {persona: "Vargiu", materia: "Matematica Discreta", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Martedì", inizio: "11:00", fine: "12:45"},
+        {persona: "Vargiu", materia: "Algoritmi e Strutture Dati", aula: "Aula F / Laboratorio T", edificio: "Palazzo delle Scienze", giorno: "Martedì", inizio: "15:00", fine: "17:30"},
+        {persona: "Vargiu", materia: "Matematica Discreta", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Mercoledì", inizio: "11:00", fine: "12:45"},
+        {persona: "Vargiu", materia: "Algoritmi e Strutture Dati", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Giovedì", inizio: "9:00", fine: "10:45"},
+        {persona: "Vargiu", materia: "Architettura degli Elaboratori", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Giovedì", inizio: "11:00", fine: "13:30"},
+        {persona: "Vargiu", materia: "Algoritmi e Strutture Dati", aula: "Aula Costa", edificio: "Aula Costa", giorno: "Venerdì", inizio: "9:00", fine: "10:45"},
+        {persona: "Vargiu", materia: "Architettura degli Elaboratori", aula: "Aula Costa", edificio: "Aula Costa", giorno: "Venerdì", inizio: "11:00", fine: "12:45"},
     ];
     return out;
 }
