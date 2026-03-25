45f330129af4a6b5aa8a20e67c7ec9360616a7be
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2026-03-25 12:45:39 +0100

    Corretto orario CSMN

--- orari/Script/dati.js
+++ orari/Script/dati.js
@@ -5,7 +5,7 @@ function getDati() {
         {persona: "Lorenzo", materia: "Ingegneria del Software Ex", aula: "Laboratorio T", edificio: "Palazzo delle Scienze", giorno: "Martedì", inizio: "11:00", fine: "13:00"},
         {persona: "Lorenzo", materia: "Ingegneria del Software", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Mercoledì", inizio: "9:00", fine: "10:45"},
         {persona: "Lorenzo", materia: "Istituzioni di Logica", aula: "Aula 17", edificio: "Sa Duchessa", giorno: "Mercoledì", inizio: "11:40", fine: "13:05"},
-        {persona: "Lorenzo", materia: "Calcolo Scientifico e Metodi Numerici", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Mercoledì", inizio: "14:30", fine: "16:15"},
+        {persona: "Lorenzo", materia: "Calcolo Scientifico e Metodi Numerici", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Mercoledì", inizio: "15:00", fine: "16:45"},
         {persona: "Lorenzo", materia: "Istituzioni di Logica", aula: "Aula 17", edificio: "Sa Duchessa", giorno: "Venerdì", inizio: "9:15", fine: "10:50"},
         {persona: "Lorenzo", materia: "Calcolo Scientifico e Metodi Numerici Ex", aula: "Laboratorio T", edificio: "Palazzo delle Scienze", giorno: "Venerdì", inizio: "11:30", fine: "13:15"},
         {persona: "Vargiu", materia: "Matematica Discreta", aula: "Aula Magna di Matematica", edificio: "Palazzo delle Scienze", giorno: "Lunedì", inizio: "9:00", fine: "10:45"},
