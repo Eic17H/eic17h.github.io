27c056d183ca9e635823a1185bd418769ff4cc4c
Lorenzo Derosas <139933730+Eic17H@users.noreply.github.com>
2026-09-11 19:09:30 +0200

    giochino numegriglia

---
+++ numegriglia/main.js
@@ -0,0 +1,81 @@
+var current = 1;
+var time=0;
+var playing=true;
+const delay = ms => new Promise(res => setTimeout(res, ms));
+var currentAttempt = {progressAtTargetTime: []};
+const targetTime = 30;
+var timestamp;
+
+function load() {
+    time=0;
+    prepareGame();
+    timer();
+}
+
+function getRandomOrder() {
+    let numbers = [];
+    for (let i=0; i<36; i++) {
+        numbers[i] = i+1;
+    }
+    for (let i=35; i>=0; i--) {
+        let temp = numbers[i];
+        let index = Math.floor(Math.random()*36);
+        numbers[i] = numbers[index];
+        numbers[index] = temp;
+    }
+    return numbers;
+}
+
+function prepareGame() {
+
+    let numbers = getRandomOrder();
+    currentAttempt = {progressAtTargetTime: []};
+    timestamp = new Date();
+
+    // Making buttons
+    let gamespace = document.getElementById("gamespace");
+    gamespace.innerHTML = "";
+    let k=0;
+    for (let i=0; i<6; i++) {
+        let thisrow = `<div class="gamerow" id="gamerow-${i}">`
+        for (let j=0; j<6; j++) {
+            thisrow += `<button class="gamesquare" id="gamesquare-${i};${j}" onclick="playermove(${numbers[k]})">${numbers[k]}</button>`;
+            k++;
+        }
+        thisrow += "</div>"
+        gamespace.innerHTML += thisrow;
+    }
+    current = 1;
+}
+
+function playermove(k) {
+    if(k!=current) lose();
+    else if(k==current) {current++;
+    if(current>36) win();}
+}
+
+function lose() {
+    window.alert("Lose")
+    load();
+}
+
+function win() {
+    window.alert("Win in "+time+"s")
+    currentAttempt.winTime = time;
+    document.getElementById("30sscore").innerHTML = `tot: ${time}s`;
+    load();
+}
+
+async function timer() {
+    let box = document.getElementById("timer")
+    while(playing) {
+        await delay(10);
+        time += 0.01;
+        time = Math.round(time*100)/100;
+        box.innerHTML = time+"s"
+        if(time == targetTime) {
+            currentAttempt.progressAtTargetTime[targetTime] = current;
+            document.getElementById("30sscore").innerHTML = `${targetTime}s: ${current} punti`;
+        }
+    }
+}
